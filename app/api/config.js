bcc7e7c4c1d540eb88da6fbc68b23a34bf385d25
jekos23 <zenaivano64@gmail.com>
2024-05-30 20:13:25 +0300

    3

--- app/api/config.js
+++ app/api/config.js
@@ -1,4 +1,4 @@
-export const BASE_URL = 'https://jekos.frontend.spb.nomorepartiesco.ru'
+export const BASE_URL = 'https://jekos52.spb.nomorepartiesco.ru'
 
 export const endpoints = {
   games: `${BASE_URL}/games`,
